635b7a54fb09fcd8e858bdfb016046cdf677ab38
Dan K. Snelson <dan@snelson.us>
2018-06-28 06:23:11 -0500

    Update inventory with LDAP lookup
    Requirements: Enterprise Connect

---
+++ Update Inventory.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+########################################################################################################
+# Update inventory with endUsername. See:                                                              #
+# http://docs.jamf.com/10.5.0/jamf-pro/administrator-guide/Computer_Inventory_Collection_Settings.html #
+########################################################################################################
+
+echo "*** Updating inventory ***"
+
+# Get the logged in users username
+loggedInUser=$( /usr/bin/stat -f %Su "/dev/console" )
+
+if [[ ${loggedInUser} == "root" ]] || [[ ${loggedInUser} == "adobeinstall" ]] || [[ ${loggedInUser} == "_mbsetupuser" ]] ; then
+
+	echo "${loggedInUser} is currently the logged-in user; starting normal inventory update ..."
+
+	/usr/local/jamf/bin/jamf recon
+
+else
+
+	if [[ -f "/Applications/Enterprise Connect.app/Contents/SharedSupport/eccl" ]] ; then
+
+		adUsername=$( /usr/bin/su \- "${loggedInUser}" -c "/Applications/Enterprise\ Connect.app/Contents/SharedSupport/eccl -p adUsername" | /usr/bin/sed 's/adUsername: //' )
+
+		if [[ ${adUsername} == "missing value" ]]; then	# Enterprise Connect installed, but user is NOT logged in
+
+			echo "${loggedInUser} NOT logged into Enterprise Connect; Starting inventory update for logged-in user ${loggedInUser}  ..."
+
+			/usr/local/jamf/bin/jamf recon -endUsername ${loggedInUser}
+
+			exit 0
+
+		else	# Enterprise Connect installed and the user is logged in
+
+			echo "Starting inventory update for Enterprise Connect user ${adUsername} ..."
+
+			/usr/local/jamf/bin/jamf recon -endUsername ${adUsername}
+
+			exit 0
+
+		fi
+
+else
+
+		echo "Enterprise Connect NOT installed; Starting inventory update for the logged-in user ${loggedInUser} ..."
+
+		/usr/local/jamf/bin/jamf recon -endUsername ${loggedInUser}
+
+		exit 0
+
+	fi
+
+fi
+
+
+exit 0
