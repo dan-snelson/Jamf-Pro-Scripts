1c716b4e6ec2195c288e16e57e00e03eb2f1428f
Dan K. Snelson <dan@snelson.us>
2021-01-16 16:11:15 -0700

    v1.4

--- Disk Usage Home Directory.sh
+++ Disk Usage Home Directory.sh
@@ -15,8 +15,9 @@
 #	Version 1.2, 4-Jan-2017, Dan K. Snelson
 #		Updated for macOS 10.12
 #	Version 1.3, 4-Jul-2018, Dan K. Snelson
-#		Updated with df; thanks to seann and EdLuo
-#		See: https://www.jamf.com/jamf-nation/discussions/14881/
+#		Updated for macOS 10.13
+#	Version 1.4, 11-Nov-2020, Dan K. Snelson
+#		Added Time Machine Local Snapshot Information
 #
 ####################################################################################################
 
@@ -26,24 +27,91 @@ loggedInUser=$( /usr/bin/stat -f%Su /dev/console )
 loggedInUserHome=$( /usr/bin/dscl . -read /Users/$loggedInUser NFSHomeDirectory | /usr/bin/awk '{print $NF}' ) # mm2270
 machineName=$( /usr/sbin/scutil --get LocalHostName )
 volumeName=$( /usr/sbin/diskutil info / | /usr/bin/grep "Volume Name:" | /usr/bin/awk '{print $3,$4}' )
-availableSpace=$( /bin/df -g / | /usr/bin/awk 'FNR==2{print $4}' )
-totalSpace=$( /bin/df -g / | /usr/bin/awk 'FNR==2{print $2}' )
-percentageAvailable=$( /bin/echo "scale=3; ($availableSpace / $totalSpace) * 100" | /usr/bin/bc )
-outputFileName="$loggedInUserHome/Desktop/$loggedInUser-DiskUsage.txt"
+FreeSpace=$( /usr/sbin/diskutil info / | /usr/bin/grep  -E 'Free Space|Available Space|Container Free Space' | /usr/bin/awk -F ":\s*" '{ print $2 }' | awk -F "(" '{ print $1 }' | xargs )
+FreeBytes=$( /usr/sbin/diskutil info / | /usr/bin/grep -E 'Free Space|Available Space|Container Free Space' | /usr/bin/awk -F "(\\\(| Bytes\\\))" '{ print $2 }' )
+DiskBytes=$( /usr/sbin/diskutil info / | /usr/bin/grep -E 'Total Space' | /usr/bin/awk -F "(\\\(| Bytes\\\))" '{ print $2 }' )
+FreePercentage=$(echo "scale=2; $FreeBytes*100/$DiskBytes" | bc)
+diskSpace="$FreeSpace free (${FreePercentage}% available)"
+outputFileName="$loggedInUserHome/Desktop/$loggedInUser-DiskUsage-`date '+%Y-%m-%d-%H%M%S'`.txt"
 
+# Output to Terminal
+echo "### Disk usage for \"$loggedInUserHome\" for volume \"$volumeName\" on computer \"$machineName\"  ###"
+echo "Disk Space: ${diskSpace}"
+echo "Report Location: $outputFileName"
 
 
 # Output to user
-/bin/echo "--------------------------------------------------------------------------------------------------------------" > $outputFileName
-/bin/echo "`now` Disk usage for \"$loggedInUserHome\" for volume \"$volumeName\" on computer \"$machineName\" " >> $outputFileName
-/bin/echo "* Available Space:	$availableSpace GB" >> $outputFileName
-/bin/echo "* Total Space:		$totalSpace GB" >> $outputFileName
-/bin/echo "* Percentage Free: 	$percentageAvailable%" >> $outputFileName
-/bin/echo "--------------------------------------------------------------------------------------------------------------" >> $outputFileName
+/bin/echo "-----------------------------------------------------------------------------------------------------------------------------------------" > $outputFileName
+/bin/echo "Disk usage for \"$loggedInUserHome\" for volume \"$volumeName\" on computer \"$machineName\" " >> $outputFileName
+/bin/echo "Disk Space: ${diskSpace}" >> $outputFileName
+/bin/echo "Report Location: $outputFileName" >> $outputFileName
+/bin/echo "-----------------------------------------------------------------------------------------------------------------------------------------" >> $outputFileName
+/bin/echo " " >> $outputFileName
+/bin/echo " " >> $outputFileName
 /bin/echo " " >> $outputFileName
 /bin/echo "GBs	Directory or File" >> $outputFileName
 /bin/echo " " >> $outputFileName
-/usr/bin/du -axrg $loggedInUserHome | /usr/bin/sort -nr | /usr/bin/head -n 75 >> $outputFileName
+/usr/bin/su - $loggedInUser -c "/usr/bin/du -axrg $loggedInUserHome 2>/dev/null | /usr/bin/sort -nr | /usr/bin/head -n 50" >> $outputFileName
+/bin/echo " " >> $outputFileName
+/bin/echo "-----------------------------------------------------------------------------------------------------------------------------------------" >> $outputFileName
+
+
+
+# Time Machine Local Snapshot Information
+
+/bin/echo "
+###
+# Time Machine Information
+###
+" >> $outputFileName
+
+tmDestinationInfo=$( /usr/bin/tmutil destinationinfo )
+
+if [[ "${tmDestinationInfo}" == *"No destinations configured"* ]]; then
+
+	# Time Machine destination NOT configured.
+	/bin/echo "WARNING: Time Machine destination NOT configured." >> $outputFileName
+
+else
+
+	# List Time Machine Local Snapshots
+	#/bin/echo "Time Machine Local Snapshots:" >> $outputFileName
+	/usr/bin/tmutil listlocalsnapshots / >> $outputFileName
+
+	/bin/echo "
+---
+- Thin Local Time Machine Snapshots
+---
+
+Thinning local Time Machine snapshots can quickly free up disk space by PERMANENTLY deleting local Time Machine snapshots.
+
+man tmutil
+
+	thinlocalsnapshots mount_point [purge_amount] [urgency]
+
+		Thin local Time Machine snapshots for the specified volume.
+
+		When purge_amount and urgency are specified, tmutil will attempt (with urgency level 1-4)
+		to reclaim purge_amount in bytes by thinning snapshots.
+
+		If urgency is not specified, the default urgency will be used.
+
+
+
+ABSOLUTELY UNSUPPORTED EXAMPLES TO BE USED AT YOUR OWN RISK:
+
+# Free 20 GB of snapshots stored on the boot drive (with maximum urgency)
+tmutil thinlocalsnapshots / 21474836480 4
+
+# Free 36 GB of snapshots stored on the boot drive (with maximum urgency)
+tmutil thinlocalsnapshots / 38654705664 4
+
+
+
+" >> $outputFileName
+
+fi
+
 
 
 if [ -f $outputFileName ]; then	# Open in Safari
--- Disk Usage Root Volume.sh
+++ Disk Usage Root Volume.sh
@@ -3,7 +3,7 @@
 #
 # ABOUT
 #
-#	Disk Usage: Root Volume
+#	Disk Usage: Home Directory
 #
 ####################################################################################################
 #
@@ -15,10 +15,12 @@
 #	Version 1.2, 4-Jan-2017, Dan K. Snelson
 #		Updated for macOS 10.12
 #	Version 1.3, 4-Jul-2018, Dan K. Snelson
-#		Updated with df; thanks to seann and EdLuo
-#		See: https://www.jamf.com/jamf-nation/discussions/14881/
+#		Updated for macOS 10.13
+#	Version 1.4, 11-Nov-2020, Dan K. Snelson
+#		Updates from Disk Usage Home Directory
 #
 ####################################################################################################
+
 
 
 # Variables
@@ -26,24 +28,36 @@ loggedInUser=$( /usr/bin/stat -f%Su /dev/console )
 loggedInUserHome=$( /usr/bin/dscl . -read /Users/$loggedInUser NFSHomeDirectory | /usr/bin/awk '{print $NF}' ) # mm2270
 machineName=$( /usr/sbin/scutil --get LocalHostName )
 volumeName=$( /usr/sbin/diskutil info / | /usr/bin/grep "Volume Name:" | /usr/bin/awk '{print $3,$4}' )
-availableSpace=$( /bin/df -g / | /usr/bin/awk 'FNR==2{print $4}' )
-totalSpace=$( /bin/df -g / | /usr/bin/awk 'FNR==2{print $2}' )
-percentageAvailable=$( /bin/echo "scale=3; ($availableSpace / $totalSpace) * 100" | /usr/bin/bc )
-outputFileName="$loggedInUserHome/Desktop/$machineName-ComputerDiskUsage.txt"
+FreeSpace=$( /usr/sbin/diskutil info / | /usr/bin/grep  -E 'Free Space|Available Space|Container Free Space' | /usr/bin/awk -F ":\s*" '{ print $2 }' | awk -F "(" '{ print $1 }' | xargs )
+FreeBytes=$( /usr/sbin/diskutil info / | /usr/bin/grep -E 'Free Space|Available Space|Container Free Space' | /usr/bin/awk -F "(\\\(| Bytes\\\))" '{ print $2 }' )
+DiskBytes=$( /usr/sbin/diskutil info / | /usr/bin/grep -E 'Total Space' | /usr/bin/awk -F "(\\\(| Bytes\\\))" '{ print $2 }' )
+FreePercentage=$(echo "scale=2; $FreeBytes*100/$DiskBytes" | bc)
+diskSpace="$FreeSpace free (${FreePercentage}% available)"
+outputFileName="$loggedInUserHome/Desktop/$machineName-ComputerDiskUsage-`date '+%Y-%m-%d-%H%M%S'`.txt"
 
 
+
+# Output to Terminal
+echo "### Disk usage for volume \"$volumeName\" on computer \"$machineName\"  ###"
+echo "Disk Space: $diskSpace"
+echo "Report Location: $outputFileName"
 
 # Output to user
-/bin/echo "--------------------------------------------------------------------------------------------------------------" > $outputFileName
-/bin/echo "`now` Disk usage for \"$volumeName\" on computer \"$machineName\"" >> $outputFileName
-/bin/echo "* Available Space:	$availableSpace GB" >> $outputFileName
-/bin/echo "* Total Space:		$totalSpace GB" >> $outputFileName
-/bin/echo "* Percentage Free: 	$percentageAvailable%" >> $outputFileName
-/bin/echo "--------------------------------------------------------------------------------------------------------------" >> $outputFileName
+/bin/echo "-----------------------------------------------------------------------------------------------------------------------------------------" > $outputFileName
+/bin/echo "Disk usage for \"$loggedInUserHome\" for volume \"$volumeName\" on computer \"$machineName\" " >> $outputFileName
+/bin/echo "Disk Space: $diskSpace" >> $outputFileName
+/bin/echo "Report Location: $outputFileName" >> $outputFileName
+/bin/echo "-----------------------------------------------------------------------------------------------------------------------------------------" >> $outputFileName
+/bin/echo " " >> $outputFileName
+/bin/echo " " >> $outputFileName
 /bin/echo " " >> $outputFileName
 /bin/echo "GBs	Directory or File" >> $outputFileName
 /bin/echo " " >> $outputFileName
-/usr/bin/du -axrg / | /usr/bin/sort -nr | /usr/bin/head -n 75 >> $outputFileName
+/usr/bin/du -axrg / 2>/dev/null | /usr/bin/sort -nr | /usr/bin/head -n 75 >> $outputFileName
+/bin/echo " " >> $outputFileName
+/bin/echo "-----------------------------------------------------------------------------------------------------------------------------------------" >> $outputFileName
+
+
 
 if [ -f $outputFileName ]; then	# Open in Safari
 	/usr/bin/su - $loggedInUser -c "open -a safari $outputFileName"
