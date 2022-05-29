6db49b134b4fcd68e16e360be0648375b4580c9f
Dan K. Snelson <dan@snelson.us>
2022-05-28 20:18:22 -0600

    v1.5

--- Disk Usage Home Directory.sh
+++ Disk Usage Home Directory.sh
@@ -3,30 +3,48 @@
 #
 # ABOUT
 #
-#	Disk Usage: Home Directory
+#   Disk Usage: Home Directory
 #
 ####################################################################################################
 #
 # HISTORY
 #
-#	Version 1.0, 8-Dec-2014, Dan K. Snelson
-#	Version 1.1, 8-Jun-2015, Dan K. Snelson
-#		See: https://jamfnation.jamfsoftware.com/discussion.html?id=14701
-#	Version 1.2, 4-Jan-2017, Dan K. Snelson
-#		Updated for macOS 10.12
-#	Version 1.3, 4-Jul-2018, Dan K. Snelson
-#		Updated for macOS 10.13
-#	Version 1.4, 11-Nov-2020, Dan K. Snelson
-#		Added Time Machine Local Snapshot Information
+#   Version 1.0, 8-Dec-2014, Dan K. Snelson (@dan-snelson) (@dan-snelson)
+#       Original version
+#   Version 1.1, 8-Jun-2015, Dan K. Snelson (@dan-snelson)
+#       See: https://jamfnation.jamfsoftware.com/discussion.html?id=14701
+#   Version 1.2, 4-Jan-2017, Dan K. Snelson (@dan-snelson)
+#       Updated for macOS 10.12
+#   Version 1.3, 4-Jul-2018, Dan K. Snelson (@dan-snelson)
+#       Updated for macOS 10.13
+#   Version 1.4, 11-Nov-2020, Dan K. Snelson (@dan-snelson)
+#       Added Time Machine Local Snapshot Information
+#   Version 1.5, 28-May-2022, Dan K. Snelson (@dan-snelson)
+#       See: https://snelson.us/2022/05/disk-usage-report-monterey-compatible/
 #
 ####################################################################################################
 
 
+
+####################################################################################################
+#
 # Variables
-loggedInUser=$( /usr/bin/stat -f%Su /dev/console )
+#
+####################################################################################################
+
+scriptVersion="1.5"
+scriptResult="Version ${scriptVersion}; "
+loggedInUser=$( /bin/echo "show State:/Users/ConsoleUser" | /usr/sbin/scutil | /usr/bin/awk '/Name :/ { print $3 }' )
 loggedInUserHome=$( /usr/bin/dscl . -read /Users/$loggedInUser NFSHomeDirectory | /usr/bin/awk '{print $NF}' ) # mm2270
 machineName=$( /usr/sbin/scutil --get LocalHostName )
 volumeName=$( /usr/sbin/diskutil info / | /usr/bin/grep "Volume Name:" | /usr/bin/awk '{print $3,$4}' )
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Calculate Free Space
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 FreeSpace=$( /usr/sbin/diskutil info / | /usr/bin/grep  -E 'Free Space|Available Space|Container Free Space' | /usr/bin/awk -F ":\s*" '{ print $2 }' | awk -F "(" '{ print $1 }' | xargs )
 FreeBytes=$( /usr/sbin/diskutil info / | /usr/bin/grep -E 'Free Space|Available Space|Container Free Space' | /usr/bin/awk -F "(\\\(| Bytes\\\))" '{ print $2 }' )
 DiskBytes=$( /usr/sbin/diskutil info / | /usr/bin/grep -E 'Total Space' | /usr/bin/awk -F "(\\\(| Bytes\\\))" '{ print $2 }' )
@@ -34,30 +52,90 @@ FreePercentage=$(echo "scale=2; $FreeBytes*100/$DiskBytes" | bc)
 diskSpace="$FreeSpace free (${FreePercentage}% available)"
 outputFileName="$loggedInUserHome/Desktop/$loggedInUser-DiskUsage-`date '+%Y-%m-%d-%H%M%S'`.txt"
 
-# Output to Terminal
+
+
+####################################################################################################
+#
+# Functions
+#
+####################################################################################################
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# JAMF Display Message
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+function jamfDisplayMessage() {
+    echo "${1}"
+    scriptResult+="${1}; "
+    /usr/local/jamf/bin/jamf displayMessage -message "${1}" &
+}
+
+
+
+####################################################################################################
+#
+# Program
+#
+####################################################################################################
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Logging preamble
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+echo "Disk Usage: Home Directory (${scriptVersion})"
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Display message
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+jamfDisplayMessage "Please wait about three minutes while the disk usage for \"$loggedInUser\" is analyzed …"
+
+
+
+
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Output to log
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 echo "### Disk usage for \"$loggedInUserHome\" for volume \"$volumeName\" on computer \"$machineName\"  ###"
-echo "Disk Space: ${diskSpace}"
+echo "Disk Space: $diskSpace"
 echo "Report Location: $outputFileName"
 
 
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Output to user
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 /bin/echo "-----------------------------------------------------------------------------------------------------------------------------------------" > $outputFileName
 /bin/echo "Disk usage for \"$loggedInUserHome\" for volume \"$volumeName\" on computer \"$machineName\" " >> $outputFileName
-/bin/echo "Disk Space: ${diskSpace}" >> $outputFileName
+/bin/echo "Disk Space: $diskSpace" >> $outputFileName
 /bin/echo "Report Location: $outputFileName" >> $outputFileName
 /bin/echo "-----------------------------------------------------------------------------------------------------------------------------------------" >> $outputFileName
 /bin/echo " " >> $outputFileName
 /bin/echo " " >> $outputFileName
 /bin/echo " " >> $outputFileName
-/bin/echo "GBs	Directory or File" >> $outputFileName
+/bin/echo "GBs    Directory or File" >> $outputFileName
 /bin/echo " " >> $outputFileName
-/usr/bin/su - $loggedInUser -c "/usr/bin/du -axrg $loggedInUserHome 2>/dev/null | /usr/bin/sort -nr | /usr/bin/head -n 50" >> $outputFileName
+/usr/bin/du -axrg $loggedInUserHome | /usr/bin/sort -nr | /usr/bin/head -n 50 >> $outputFileName
 /bin/echo " " >> $outputFileName
 /bin/echo "-----------------------------------------------------------------------------------------------------------------------------------------" >> $outputFileName
 
 
 
+# To Do: ~/Library/Developer/CoreSimulator
+#xcrun simctl delete unavailable
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Time Machine Local Snapshot Information
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 /bin/echo "
 ###
@@ -69,16 +147,16 @@ tmDestinationInfo=$( /usr/bin/tmutil destinationinfo )
 
 if [[ "${tmDestinationInfo}" == *"No destinations configured"* ]]; then
 
-	# Time Machine destination NOT configured.
-	/bin/echo "WARNING: Time Machine destination NOT configured." >> $outputFileName
+    # Time Machine destination NOT configured.
+    /bin/echo "WARNING: Time Machine destination NOT configured." >> $outputFileName
 
 else
 
-	# List Time Machine Local Snapshots
-	#/bin/echo "Time Machine Local Snapshots:" >> $outputFileName
-	/usr/bin/tmutil listlocalsnapshots / >> $outputFileName
+    # List Time Machine Local Snapshots
+    #/bin/echo "Time Machine Local Snapshots:" >> $outputFileName
+    /usr/bin/tmutil listlocalsnapshots / >> $outputFileName
 
-	/bin/echo "
+    /bin/echo "
 ---
 - Thin Local Time Machine Snapshots
 ---
@@ -87,14 +165,14 @@ Thinning local Time Machine snapshots can quickly free up disk space by PERMANEN
 
 man tmutil
 
-	thinlocalsnapshots mount_point [purge_amount] [urgency]
+    thinlocalsnapshots mount_point [purge_amount] [urgency]
 
-		Thin local Time Machine snapshots for the specified volume.
+        Thin local Time Machine snapshots for the specified volume.
 
-		When purge_amount and urgency are specified, tmutil will attempt (with urgency level 1-4)
-		to reclaim purge_amount in bytes by thinning snapshots.
+        When purge_amount and urgency are specified, tmutil will attempt (with urgency level 1-4)
+        to reclaim purge_amount in bytes by thinning snapshots.
 
-		If urgency is not specified, the default urgency will be used.
+        If urgency is not specified, the default urgency will be used.
 
 
 
@@ -114,9 +192,20 @@ fi
 
 
 
-if [ -f $outputFileName ]; then	# Open in Safari
-	/usr/bin/su - $loggedInUser -c "open -a safari $outputFileName"
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Open in Safari
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+if [ -f $outputFileName ]; then
+    /usr/bin/su - $loggedInUser -c "open -a safari $outputFileName"
 fi
 
-exit 0		## Success
-exit 1		## Failure
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Exit
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+scriptResult+="End-of-line"
+echo ${scriptResult}
+
+exit 0        ## Success
+exit 1        ## Failure
--- Disk Usage Root Volume.sh
+++ Disk Usage Root Volume.sh
@@ -3,31 +3,47 @@
 #
 # ABOUT
 #
-#	Disk Usage: Home Directory
+#    Disk Usage: Root Volume
 #
 ####################################################################################################
 #
 # HISTORY
 #
-#	Version 1.0, 8-Dec-2014, Dan K. Snelson
-#	Version 1.1, 8-Jun-2015, Dan K. Snelson
-#		See: https://jamfnation.jamfsoftware.com/discussion.html?id=14701
-#	Version 1.2, 4-Jan-2017, Dan K. Snelson
-#		Updated for macOS 10.12
-#	Version 1.3, 4-Jul-2018, Dan K. Snelson
-#		Updated for macOS 10.13
-#	Version 1.4, 11-Nov-2020, Dan K. Snelson
-#		Updates from Disk Usage Home Directory
+#   Version 1.0, 8-Dec-2014, Dan K. Snelson (@dan-snelson) (@dan-snelson)
+#       Original version
+#   Version 1.1, 8-Jun-2015, Dan K. Snelson (@dan-snelson)
+#       See: https://jamfnation.jamfsoftware.com/discussion.html?id=14701
+#   Version 1.2, 4-Jan-2017, Dan K. Snelson (@dan-snelson)
+#       Updated for macOS 10.12
+#   Version 1.3, 4-Jul-2018, Dan K. Snelson (@dan-snelson)
+#       Updated for macOS 10.13
+#   Version 1.4, 11-Nov-2020, Dan K. Snelson (@dan-snelson)
+#       Increased version number to match "Disk Usage Home Directory"
+#   Version 1.5, 28-May-2022, Dan K. Snelson (@dan-snelson)
+#       See: https://snelson.us/2022/05/disk-usage-report-monterey-compatible/
 #
 ####################################################################################################
 
 
 
+####################################################################################################
+#
 # Variables
-loggedInUser=$( /usr/bin/stat -f%Su /dev/console )
+#
+####################################################################################################
+
+scriptVersion="1.5"
+scriptResult="Version ${scriptVersion}; "
+loggedInUser=$( /bin/echo "show State:/Users/ConsoleUser" | /usr/sbin/scutil | /usr/bin/awk '/Name :/ { print $3 }' )
 loggedInUserHome=$( /usr/bin/dscl . -read /Users/$loggedInUser NFSHomeDirectory | /usr/bin/awk '{print $NF}' ) # mm2270
 machineName=$( /usr/sbin/scutil --get LocalHostName )
 volumeName=$( /usr/sbin/diskutil info / | /usr/bin/grep "Volume Name:" | /usr/bin/awk '{print $3,$4}' )
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Calculate Free Space
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 FreeSpace=$( /usr/sbin/diskutil info / | /usr/bin/grep  -E 'Free Space|Available Space|Container Free Space' | /usr/bin/awk -F ":\s*" '{ print $2 }' | awk -F "(" '{ print $1 }' | xargs )
 FreeBytes=$( /usr/sbin/diskutil info / | /usr/bin/grep -E 'Free Space|Available Space|Container Free Space' | /usr/bin/awk -F "(\\\(| Bytes\\\))" '{ print $2 }' )
 DiskBytes=$( /usr/sbin/diskutil info / | /usr/bin/grep -E 'Total Space' | /usr/bin/awk -F "(\\\(| Bytes\\\))" '{ print $2 }' )
@@ -37,31 +53,92 @@ outputFileName="$loggedInUserHome/Desktop/$machineName-ComputerDiskUsage-`date '
 
 
 
-# Output to Terminal
+####################################################################################################
+#
+# Functions
+#
+####################################################################################################
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# JAMF Display Message
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+function jamfDisplayMessage() {
+    echo "${1}"
+    scriptResult+="${1}; "
+    /usr/local/jamf/bin/jamf displayMessage -message "${1}" &
+}
+
+
+
+####################################################################################################
+#
+# Program
+#
+####################################################################################################
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Logging preamble
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+echo "Disk Usage: Root Volume (${scriptVersion})"
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Display message
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+jamfDisplayMessage "Please wait 15 minutes (or more) while the disk usage for the entire \"$volumeName\" is analyzed."
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Output to log
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 echo "### Disk usage for volume \"$volumeName\" on computer \"$machineName\"  ###"
 echo "Disk Space: $diskSpace"
 echo "Report Location: $outputFileName"
 
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Output to user
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 /bin/echo "-----------------------------------------------------------------------------------------------------------------------------------------" > $outputFileName
-/bin/echo "Disk usage for \"$loggedInUserHome\" for volume \"$volumeName\" on computer \"$machineName\" " >> $outputFileName
+/bin/echo "Disk usage for volume \"$volumeName\" on computer \"$machineName\" " >> $outputFileName
 /bin/echo "Disk Space: $diskSpace" >> $outputFileName
 /bin/echo "Report Location: $outputFileName" >> $outputFileName
 /bin/echo "-----------------------------------------------------------------------------------------------------------------------------------------" >> $outputFileName
 /bin/echo " " >> $outputFileName
 /bin/echo " " >> $outputFileName
 /bin/echo " " >> $outputFileName
-/bin/echo "GBs	Directory or File" >> $outputFileName
+/bin/echo "GBs    Directory or File" >> $outputFileName
 /bin/echo " " >> $outputFileName
-/usr/bin/du -axrg / 2>/dev/null | /usr/bin/sort -nr | /usr/bin/head -n 75 >> $outputFileName
+/usr/bin/du -axrg / 2>&1 | /usr/bin/sort -nr | /usr/bin/head -n 50 >> $outputFileName
 /bin/echo " " >> $outputFileName
 /bin/echo "-----------------------------------------------------------------------------------------------------------------------------------------" >> $outputFileName
 
 
 
-if [ -f $outputFileName ]; then	# Open in Safari
-	/usr/bin/su - $loggedInUser -c "open -a safari $outputFileName"
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Open in Safari
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+if [ -f $outputFileName ]; then
+    /usr/bin/su - $loggedInUser -c "open -a safari $outputFileName"
 fi
 
-exit 0		## Success
-exit 1		## Failure
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Exit
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+scriptResult+="End-of-line"
+echo ${scriptResult}
+
+exit 0        ## Success
+exit 1        ## Failure
