93381674e8ee67980c5c3628217438719050fe06
Dan K. Snelson <dan@snelson.us>
2024-09-05 06:05:26 -0400

    Proof-of-concept Script
    Signed-off-by: Dan K. Snelson <dan@snelson.us>

---
+++ EDR/edrScript.zsh
@@ -0,0 +1,215 @@
+#!/bin/zsh --no-rcs 
+# shellcheck shell=bash
+
+####################################################################################################
+#
+# EDR Script
+#
+#   Purpose: EDR Script
+#
+####################################################################################################
+#
+# HISTORY
+#
+# Version 0.0.1, 05-Sep-2024, Dan K. Snelson (@dan-snelson)
+#   - Original, proof-of-concept version
+#
+####################################################################################################
+#
+# Global Variables
+#
+####################################################################################################
+
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin/
+
+# Script Version
+scriptVersion="0.0.1"
+
+# Client-side Log
+scriptLog="/var/log/org.test.edr.log"
+
+# Script Modification Timestamp (i.e., $( date '+%Y-%m-%d-%H%M%S' ) )
+scriptModificationTimestamp="2024-09-05-050308"
+
+# Initialize SECONDS
+SECONDS="0"
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Jamf Pro Script Parameters
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Parameter 4: Operation Mode [ debug | normal | verbose ]
+operationMode="${4:-"verbose"}"
+
+# Parameter 5: "Anticipation" Duration (in seconds)
+anticipationDuration="${5:-"3"}"
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Organization Variables
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Script Human-readabale Name
+humanReadableScriptName="EDR Script"
+
+# Organization's Script Name
+organizationScriptName="EDR"
+
+
+
+####################################################################################################
+#
+# Functions
+#
+####################################################################################################
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Client-side Logging
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+function updateScriptLog() {
+    echo "${organizationScriptName} ($scriptVersion): $( date +%Y-%m-%d\ %H:%M:%S ) - ${1}" | tee -a "${scriptLog}"
+}
+
+function preFlight() {
+    updateScriptLog "[PRE-FLIGHT]      ${1}"
+}
+
+function logComment() {
+    updateScriptLog "                  ${1}"
+}
+
+function notice() {
+    updateScriptLog "[NOTICE]          ${1}"
+}
+
+function info() {
+    updateScriptLog "[INFO]            ${1}"
+}
+
+function debug() {
+    if [[ "$operationMode" == "debug" ]]; then
+        updateScriptLog "[DEBUG]           ${1}"
+    fi
+}
+
+function errorOut(){
+    updateScriptLog "[ERROR]           ${1}"
+}
+
+function error() {
+    updateScriptLog "[ERROR]           ${1}"
+    let errorCount++
+}
+
+function warning() {
+    updateScriptLog "[WARNING]         ${1}"
+    let errorCount++
+}
+
+function fatal() {
+    updateScriptLog "[FATAL ERROR]     ${1}"
+    exit 1
+}
+
+function quitOut(){
+    updateScriptLog "[QUIT]            ${1}"
+}
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Check for running processes (supplied as Parameter 1)
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+function procesStatus() {
+
+    processToCheck="${1}"
+    logComment "Process: ${processToCheck}"
+    processToCheckStatus=$( /usr/bin/pgrep -x "${processToCheck}" )
+    if [[ -n ${processToCheckStatus} ]]; then
+        processCheckResult+="'${processToCheck}' running; "
+    else
+        processCheckResult+="'${processToCheck}' NOT running; "
+    fi
+
+}
+
+
+
+####################################################################################################
+#
+# Pre-flight Checks
+#
+####################################################################################################
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Pre-flight Check: Client-side Logging
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+if [[ ! -f "${scriptLog}" ]]; then
+    touch "${scriptLog}"
+    if [[ -f "${scriptLog}" ]]; then
+        preFlight "Created specified scriptLog: ${scriptLog}"
+    else
+        fatal "Unable to create specified scriptLog '${scriptLog}'; exiting.\n\n(Is this script running as 'root' ?)"
+    fi
+else
+    preFlight "Specified scriptLog '${scriptLog}' exists; writing log entries to it"
+fi
+
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Pre-flight Check: Logging Preamble
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+preFlight "\n\n###\n# $humanReadableScriptName (${scriptVersion})\n# Operation Mode: ${operationMode}\n###\n"
+preFlight "Initiating …"
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Pre-flight Check: Confirm script is running as root
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+if [[ $(id -u) -ne 0 ]]; then
+    fatal "This script must be run as root; exiting."
+fi
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Pre-flight Check: Complete
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+preFlight "Complete!"
+
+
+
+####################################################################################################
+#
+# Program
+#
+####################################################################################################
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Proof-of-concept: `jamf about`
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+notice "Command Substitution:"
+eval "/usr/local/bin/jamf about" | tee -a "${scriptLog}"
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Exit
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+info "Elapsed Time: $(printf '%dh:%dm:%ds\n' $((SECONDS/3600)) $((SECONDS%3600/60)) $((SECONDS%60)))"
+
+quitOut "End-of-line."
